2627cd911d1d672b91dd766d0e0bc0bf76b1852e
Kevin123Ezra <130568383+Kevin123Ezra@users.noreply.github.com>
2024-05-10 06:20:43 +0400

    Update browser.py

--- browser.py
+++ browser.py
@@ -5,90 +5,124 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
 
+
+class BrowserTab(QWidget):
+    def __init__(self, parent=None):
+        super(BrowserTab, self).__init__(parent)
+        layout = QVBoxLayout()
+
+        self.browser = QWebEngineView()
+        layout.addWidget(self.browser)
+
+        self.setLayout(layout)
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.browser = QWebEngineView()
         self.setWindowTitle("OIS Browser")
-        self.setMinimumSize(QSize(1024, 750))
+        self.setMinimumSize(QSize(1350, 750))
         self.setWindowIcon(QIcon('unnamed.png'))
-        self.browser.setUrl(QUrl("https://www.google.com/?hl=en"))
-        self.setCentralWidget(self.browser)
         self.showMaximized()
 
-        self.search_history_file = 'history/search_history.txt'
-        self.load_search_history()
+        self.tabs = QTabWidget()
+        self.setCentralWidget(self.tabs)
+        self.tabs.setTabsClosable(True)
+        self.tabs.tabCloseRequested.connect(self.close_tab)
 
-        # navbar
+        self.add_tab()
+
         navbar = QToolBar()
         self.addToolBar(navbar)
+
+        # Back button
         back_btn = QAction(QIcon("icons/Arrow_back_left.png"), 'Back', self)
         back_btn.setStatusTip("Back")
-        back_btn.triggered.connect(self.browser.back)
+        back_btn.triggered.connect(self.navigate_back)
         navbar.addAction(back_btn)
 
+        # Forward button
         forward_btn = QAction(QIcon("icons/Arrow_forward_right.png"), 'Forward', self)
         forward_btn.setStatusTip("Forward")
-        forward_btn.triggered.connect(self.browser.forward)
+        forward_btn.triggered.connect(self.navigate_forward)
         navbar.addAction(forward_btn)
 
+        # Reload button
         reload_btn = QAction(QIcon("icons/Button_refresh.png"), 'Refresh', self)
         reload_btn.setStatusTip("Refresh")
-        reload_btn.triggered.connect(self.browser.reload)
+        reload_btn.triggered.connect(self.reload_page)
         navbar.addAction(reload_btn)
 
+        # Home button
         home_btn = QAction(QIcon("icons/home_icon.png"), 'Home', self)
         home_btn.setStatusTip("Home")
         home_btn.triggered.connect(self.navigate_home)
         navbar.addAction(home_btn)
 
-        find_action = QAction('Find', self)
-        find_action.setShortcut('Ctrl+F')
-        find_action.triggered.connect(self.find_text)
-        self.addAction(find_action)
+        # Secure/Unsecure site indicator
+        self.secure_icon = QLabel()
+        secure_pixmap = QPixmap("icons/safe_icon.png")
+        self.secure_icon.setPixmap(secure_pixmap)
+        self.secure_icon.setToolTip("Secure")
+        navbar.addWidget(self.secure_icon)
 
+        # URL bar
         self.url_bar = QLineEdit()
         self.url_bar.returnPressed.connect(self.navigate_to_url)
         navbar.addWidget(self.url_bar)
-        self.browser.urlChanged.connect(self.update_url)
+
+        # Add tab button
+        add_tab_btn = QAction(QIcon("icons/Add_tab_icon.png"), 'Add Tab', self)
+        add_tab_btn.setStatusTip("Add Tab")
+        add_tab_btn.triggered.connect(self.add_tab)
+        navbar.addAction(add_tab_btn)
+
+        self.tabs.currentChanged.connect(self.update_current_tab)
+
+    def add_tab(self):
+        new_tab = BrowserTab()
+        self.tabs.addTab(new_tab, "New Tab")
+        self.tabs.setCurrentWidget(new_tab)
+
+    def current_browser(self):
+        current_tab_index = self.tabs.currentIndex()
+        current_tab_widget = self.tabs.widget(current_tab_index)
+        return current_tab_widget.browser
+
+    def navigate_back(self):
+        self.current_browser().back()
+
+    def navigate_forward(self):
+        self.current_browser().forward()
+
+    def reload_page(self):
+        self.current_browser().reload()
 
     def navigate_home(self):
-        self.browser.setUrl(QUrl("https://www.google.com/?hl=en"))
+        self.current_browser().setUrl(QUrl("https://www.google.com/?hl=en"))
 
     def navigate_to_url(self):
         input_text = self.url_bar.text()
         if not input_text.startswith(("http://", "https://")):
             input_text = "https://www.google.com/search?q=" + input_text + "&hl=en"
-            self.browser.setUrl(QUrl(input_text))
+            self.current_browser().setUrl(QUrl(input_text))
         else:
-            self.browser.setUrl(QUrl(input_text))
-        self.search_history.append(input_text)
-        self.save_search_history()
+            self.current_browser().setUrl(QUrl(input_text))
 
-    def update_url(self, q):
-        self.url_bar.setText(q.toString())
+    def update_current_tab(self):
+        current_tab_index = self.tabs.currentIndex()
+        current_tab_widget = self.tabs.widget(current_tab_index)
+        current_tab_url = current_tab_widget.browser.url().toString()
+        self.url_bar.setText(current_tab_url)
 
-    def load_search_history(self):
-        if os.path.exists(self.search_history_file):
-            with open(self.search_history_file, 'r') as file:
-                self.search_history = [line.strip() for line in file.readlines()]
+    def close_tab(self, index):
+        if self.tabs.count() > 1:
+            self.tabs.removeTab(index)
         else:
-            self.search_history = []
+            QApplication.quit()
 
-    def save_search_history(self):
-        with open(self.search_history_file, 'a') as file:
-            for item in self.search_history:
-                file.write(item + '\n')
-
-    def __del__(self):
-        # Save search history before closing the application
-        self.save_search_history()
-
-    def find_text(self):
-        text, ok = QInputDialog.getText(self, 'Find Text', 'Enter text to find:')
-        if ok and text:
-            self.browser.findText(text)
 
 app = QApplication(sys.argv)
 window = MainWindow()
+window.show()
 app.exec_()
